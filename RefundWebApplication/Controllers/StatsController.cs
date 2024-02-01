3e31c0db0e08b3fcf231fc1111c8abf43554b84b
Marcin Majszczyk <majszczyk.marcin@wp.pl>
2024-02-01 19:01:57 +0100

    visual + przygotowanie pod statstyki

---
+++ RefundWebApplication/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RefundWebApplication.Controllers
+{
+    public class StatsController : Controller
+    {
+        private static int inProgressCount = 0;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateStatistics(string status)
+        {
+            if (status == "W Trakcie")
+            {
+                inProgressCount++;
+            }
+
+            // Przekierowanie na stronę z wynikami
+            return RedirectToAction("ShowStatistics");
+        }
+
+        public IActionResult ShowStatistics()
+        {
+            ViewData["InProgressCount"] = inProgressCount;
+            return View();
+        }
+    }
+}
